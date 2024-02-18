d950bc61d93616537eb555de7a8b2bf526b2aff9
Juanpa Correa <82065985+JU4NP4260@users.noreply.github.com>
2024-02-18 16:54:29 -0500

    HTTP Com

--- Assets/HttpManager.cs
+++ Assets/HttpManager.cs
@@ -1,11 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class HttpManager : MonoBehaviour
 {
-    private string fakeApiUrl = "";
+    private string fakeApiUrl = "https://my-json-server.typicode.com/JU4NP4260/Actividad2_SistDistribuidos";
+    private string RickAndMoryApiUrl = "https://rickandmortyapi.com/api";
+    private int imagesDownloadsCounter;
+
+    public List<RawImage> cardsImages = new List<RawImage>();
+    public List<TextMeshProUGUI> cardsNames = new List<TextMeshProUGUI>();
+
+    Coroutine sendRequest_GetCharacters;
+
+    public void SendRequest(int userID)
+    {
+        imagesDownloadsCounter = 0;
+        if (sendRequest_GetCharacters == null)
+        {
+            sendRequest_GetCharacters = StartCoroutine(GetUserData(userID));
+        }
+    }
 
     IEnumerator GetUserData (int uid)
     {
@@ -16,7 +34,54 @@ public class HttpManager : MonoBehaviour
         {
             Debug.Log(request.error);
         }
+        else
+        {
+            if(request.responseCode == 200)
+            {
+                UserData user = JsonUtility.FromJson<UserData>(request.downloadHandler.text);
+                Debug.Log(user.username);
+                foreach(int cardID in user.deck)
+                {
+                    StartCoroutine(GetCharacters(cardID, imagesDownloadsCounter));
+                    imagesDownloadsCounter++;
+                }
+            }
+        }
+    }
 
+    IEnumerator GetCharacters(int cardId, int deckIndex)
+    {
+        UnityWebRequest request = UnityWebRequest.Get(RickAndMoryApiUrl + "/character/" + cardId);
+        yield return request;
+
+        if (request.result == UnityWebRequest.Result.ConnectionError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            if (request.responseCode == 200)
+            {
+                Character character = JsonUtility.FromJson<Character>(request.downloadHandler.text);
+                cardsNames[deckIndex].text = character.name;
+                StartCoroutine(DownloadImage(character.image, deckIndex));              
+            }
+        }
+    }
+
+    IEnumerator DownloadImage(string url, int deckIndex)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
+        yield return request.SendWebRequest();
+
+        if(request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            cardsImages[deckIndex].texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+        }
     }
 }
 
@@ -26,4 +91,12 @@ public class UserData
     public int id;
     public string username;
     public int[] deck;
+}
+
+[System.Serializable]
+public class Character
+{
+    public int id;
+    public string name;
+    public string image;
 }
